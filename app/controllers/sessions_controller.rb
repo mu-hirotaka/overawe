7deab05cbc5863dd4a53b2433d156bb5f8a89b5b
hirop4 <hirop4@gmail.com>
2014-07-02 02:58:07 +0900

    Impl logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,21 @@
 class SessionsController < ApplicationController
   def callback
     auth = request.env['omniauth.auth']
-    user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => 'Signed in!'
+    user = User.find_by_provider_and_uid(auth['provider'], auth['uid'])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => 'Signed in!'
+    else
+      User.create_with_omniauth(auth)
+      user = User.find_by_provider_and_uid(auth['provider'], auth['uid'])
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => 'Signed in!'
+    end
 #    raise request.env['omniauth.auth'].to_yaml
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => 'Signed out!'
   end
 end
