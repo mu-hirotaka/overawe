40072188c036c1c9bfcc1f1ec0e6946f9360036c
hirop4 <hirop4@gmail.com>
2014-07-03 02:22:42 +0900

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  root :to => 'welcome#index'
+  root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#callback'
   get '/signout' => 'sessions#destroy', :as => :signout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
