cce0a0fca3c83f2b92fc463a342f2a593a15367e
hirop4 <hirop4@gmail.com>
2014-07-05 08:27:35 +0900

    Modify api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.middleware.use OmniAuth::Builder do
-  provider :twitter, '6VpCfMHNIJ6Q1gFEZ386tCTps', 'L9ypDTWVUOjiQrEBAejPGZgDcKbjtRqCrK61QEYO8xO2nq2XMh'
+  provider :twitter, '0v7RdNEE0LbY66mySu6AH8wMo', 'nRPQmisaUHwh9HtFqmK62FtmWcUsgp3jveAJ8a6jfNnJaEMXsE'
 end
