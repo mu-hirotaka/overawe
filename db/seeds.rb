ca0d8a4ed17dd695701c9168781a2ac282926f4a
hirop4 <hirop4@gmail.com>
2014-07-11 01:24:16 +0900

    Add mission(2014/7/11)

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,6 @@ MissionMaster.first_or_create([
   { id: 3, title: '最新のカメラを調査せよ', content: '最新のカメラについて調べ報告してくれ。', opened_at: 1404658800, closed_at: 1404745199 },
   { id: 4, title: 'ボールを手に入れろ', content: '種類はなんでもいい。ボールを入手してくれ。', opened_at: 1404745200, closed_at: 1404831599 },
   { id: 5, title: 'ソーメンを食べろ', content: 'ソーメンを食べて報告してくれ。', opened_at: 1404831600, closed_at: 1404917999 },
-  { id: 6, title: 'ネクタイを入手しろ', content: 'ネクタイを入手して報告しろ。', opened_at: 1404918000, closed_at: 1405004399 }
+  { id: 6, title: 'ネクタイを入手しろ', content: 'ネクタイを入手して報告しろ。', opened_at: 1404918000, closed_at: 1405004399 },
+  { id: 7, title: 'パスタを食べろ', content: 'パスタを食べて報告しろ。', opened_at: 1405004400, closed_at: 1405090799 }
 ])
