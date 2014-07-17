36d290afc064a990f617242c5e0bd5f4d069170f
hirop4 <hirop4@gmail.com>
2014-07-18 06:55:01 +0900

    Add mission(2014/07/18)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ MissionMaster.first_or_create([
   { id: 8, title: 'お茶を入手しろ', content: 'お茶を入手して報告しろ。', opened_at: 1405090800, closed_at: 1405177199 },
   { id: 9, title: '目薬を入手しろ', content: '目薬を入手して報告しろ。', opened_at: 1405177200, closed_at: 1405263599 },
   { id: 10, title: '薬を入手しろ', content: '種類はなんでもいい。薬を入手して報告しろ。', opened_at: 1405263600, closed_at: 1405349999 },
-  { id: 11, title: '本を1冊読破しろ', content: 'ジャンルは問わない。本を読破して報告しろ。', opened_at: 1405350000, closed_at: 1405609199 }
+  { id: 11, title: '本を1冊読破しろ', content: 'ジャンルは問わない。本を読破して報告しろ。', opened_at: 1405350000, closed_at: 1405609199 },
+  { id: 12, title: 'チャーハンを食え', content: 'チャーハンを食って報告しろ。', opened_at: 1405609200, closed_at: 1405695599 }
 ])
 
 
